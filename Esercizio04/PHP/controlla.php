f5ef6169d17fa8d8f633b57ef9af581733e9bd8f
Raffaele Vitale <150654623+raffaelevitale@users.noreply.github.com>
2024-11-28 13:21:12 +0100

    Upload Esercizio 04

---
+++ Esercizio04/PHP/controlla.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="it">
+
+    <head>
+        <title>Esercizio 04 PHP - Maggiore di 3 Numeri</title>
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+
+        <?php
+            // TO DO
+
+            function leggiParametri($vetPar){
+                
+            }
+
+            $n1 = 0;
+            $n2 = 0;
+            $n3 = 0;
+
+            //mi arrivano i dati dal form tramite GET
+
+            if($_SERVER["REQUEST_METHOD"] == "GET") {
+                leggiParametri($_GET);
+            }
+        ?>
+
+    </body>
+
+
+</html>
