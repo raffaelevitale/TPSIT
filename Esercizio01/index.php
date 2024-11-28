043111425c0dfb5cd0d3ca956fe06feb19f0a6b1
Raffaele Vitale <r.vitale.2756@vallauri.edu>
2024-11-28 12:44:12 +0100

    Visualizzazione del Nome

---
+++ Esercizio01/index.php
@@ -0,0 +1,8 @@
+<?php
+    $nome = "GiampyTek";
+    echo("<p>Il mio Nome è: $nome</p><br>");
+    visualizza($nome);
+    function visualizza($n){
+        echo("<p style='font-weight:bold'>Il mio Nome &egrave; $n</p>");
+    }
+?>
