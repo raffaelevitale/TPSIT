e7520ec806569c46f3d5652358664f1ebb3ede4e
Raffaele Vitale <r.vitale.2756@vallauri.edu>
2024-11-28 12:49:18 +0100

    phpinfo()

--- Esercizio02/index.php
+++ Esercizio02/index.php
@@ -1,31 +1,5 @@
-<!DOCTYPE html>
-<html lang="it">
+<?php
 
-    <head>
-        <title>Esercizio XX PHP - </title>
-        <meta charset="UTF-8">
+phpinfo();
 
-        <!-- Fogli Stile utilizzati-->
-        <link rel="stylesheet" type="text/css" href="CSS/style.css" />
-
-        <!-- JQuery Esterni alla Pagina -->
-        <script type="text/javascript" src="JQuery/JQuery371.js"></script>
-
-        <!-- JavaScript Esterni alla Pagina -->
-        <script type="text/javascript" src="JS/script.js"></script>
-
-        <!-- JavaScript Interni alla Pagina -->
-        <script type="text/javascript">
-
-        </script>
-
-
-    </head>
-
-    <body>
-        
-
-    </body>
-
-
-    </html>
+?>
